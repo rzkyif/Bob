e732d3e1e3b35c7005c642e24b08eb7d5ae7e088
Rizky I. F <rykzius@gmail.com>
2020-05-15 19:03:01 +0700

    Fixed fix

--- bob.js
+++ bob.js
@@ -126,7 +126,7 @@ async function handleEvent(event) {
       }
     } else {
       reply = "Available commands:\n";
-      reply += commands.keys().join(', ');
+      reply += Object.keys(commands).join(', ');
     }
     finalReply = [{ type: 'text', text: reply }];
   } else {
